db560a8392b50f2d69962e3aaf295e953ed722e0
R-HARSHAVARDHAN60020211 <230197.cs@rmkec.ac.in>
2026-06-18 20:50:09 +0530

    Added Singleton Pattern Exercise

---
+++ SingletonPatternExample/Logger.java
@@ -0,0 +1,9 @@
+public class Logger {
+    private static Logger instance = new Logger();
+    private Logger() {
+        System.out.println("Logger instance created");
+    }
+    public static Logger getInstance() {
+        return instance;
+    }
+}
